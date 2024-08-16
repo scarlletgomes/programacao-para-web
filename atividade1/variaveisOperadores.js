9a83b3b168eb4499b5f3e518acf2a11e17a9da79
scarlletgomes <scarllet.gomes@gmail.com>
2024-08-16 10:48:36 -0300

    atividade calculo

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,7 @@
+let n1 = 3;
+let n2 = 4;
+var resultadoSoma;
+
+resultadoSoma = n1+n2;
+
+console.log(`A soma de  é ${resultadoSoma}`);
---
+++ hello_wolrd.js
@@ -0,0 +1,14 @@
+console.log('Hello, world!');
+
+const i= 1;
+const j = 2;
+
+if(j>1){
+    const n = 3;
+}
+
+var n = 1;
+var n = 2;
+
+
+minhaStringA = 'texto1';
