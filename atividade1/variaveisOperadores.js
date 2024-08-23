c3ce49b507f3e3802ae4c592b766a9ec83e0b8af
scarlletgomes <scarllet.gomes@gmail.com>
2024-08-23 09:32:38 -0300

    atualização texto

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -10,7 +10,7 @@ resultadoSubtracao = n1-n2;
 resultadoDivisao = n1/n2;
 resultadoMutiplicacao = n1*n2;
 
-console.log(`A soma de ${n1} mais ${n2} é ${resultadoSoma}`);
-console.log(`A subtração de ${n1} mais ${n2} é ${resultadoSubtracao}`);
-console.log(`A divisão de ${n1} mais ${n2} é ${resultadoDivisao}`);
-console.log(`A mutiplicação de ${n1} mais ${n2} é ${resultadoMutiplicacao}`);
+console.log(`A soma entre ${n1} e ${n2} e ${resultadoSoma}`);
+console.log(`A subtração entre ${n1} e ${n2} é ${resultadoSubtracao}`);
+console.log(`A divisão entre ${n1} e ${n2} é ${resultadoDivisao}`);
+console.log(`A mutiplicação entre ${n1} e ${n2} é ${resultadoMutiplicacao}`);
