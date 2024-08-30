1aa0a0e6490abc47480fc314b4b842d7aaeb0f5f
scarlletgomes <scarllet.gomes@gmail.com>
2024-08-30 10:45:22 -0300

    modularização

---
+++ introdução/main.js
@@ -0,0 +1,7 @@
+const calc = require('./calculadora');
+
+let a = 2;
+let b = 3;
+
+console.log(`A soma entre ${a} e ${b} é ${calc.somar(a, b)}`);
+console.log(`A multiplicação entre ${a} e ${b} é ${calc.multiplicar(a, b)}`);
