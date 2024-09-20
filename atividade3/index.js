ee0e6e5366204f87eeaac184759aad387f6282b0
scarlletgomes <scarllet.gomes@gmail.com>
2024-09-20 10:11:53 -0300

    Correção calculadora

--- atividade3/index.js
+++ atividade3/index.js
@@ -1,16 +1,43 @@
-const cc = require ('./calc_case/calc_case');
+const calc = require ('./util/calculadora');
 
 const express = require ('express');
-
 const app = express();
 
-app.get('/somar/:a/:b', function(req, res){
-    let num = req.params.a;
-    let num2 = req.params.b;
-    res.send(cc.somar(num, num2));
+app.get('/', (req, res)=>{
+    res.send("Atividade3 - APP Calculadora");
 });
 
+app.get('/somar/:a/:b', (req, res)=>{
+    let a = req.params.a;
+    let b = req.params.b;
+    let result = calc.somar(a, b);
+    res.send(`A soma entre ${a} e ${b} é ${result}`);
+});
+
+app.get('/subtrair/:a/:b', (req, res)=>{
+    let a = req.params.a;
+    let b = req.params.b;
+    let result = calc.subtrair(a, b);
+    res.send(`A subtração entre ${a} e ${b} é ${result}`);
+});
+
+app.get('/multiplicar/:a/:b', (req, res)=>{
+    let a = req.params.a;
+    let b = req.params.b;
+    let result = calc.multiplicar(a, b);
+    res.send(`A multiplição entre ${a} e ${b} é ${result}`);
+});
+
+app.get('/dividir/:a/:b', (req, res)=>{
+    let a = req.params.a;
+    let b = req.params.b;
+    let result = calc.dividir(a, b);
+    res.send(`A divisão entre ${a} e ${b} é ${result}`);
+});
+
+
 const PORT = 8080;
+//colocar a inicialização da aplicação
 app.listen(PORT, ()=>{
     console.log("App rodando na porta " + PORT);
 });
